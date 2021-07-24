2bb6669f853b04ebdddfa08b0bd046cb5aac6efe
akhil2502 <akhilachilies>
2021-07-25 00:38:36 +0530

    [akhil] Add:Welcome

---
+++ calculater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace calculater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to calculater");
+        }
+    }
+}
